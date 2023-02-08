40b514a29b50a03bd8d6c5d768c373dbaa027d17
pizarrin737 <jmpv1924@gmail.com>
2023-02-08 15:24:55 -0600

    Se mejoró la fluidez del movimiento de la nave. Tambien se agregaron las condiciones de borde de pantalla.

--- Proyecto.py
+++ Proyecto.py
@@ -1,49 +1,34 @@
 #!/usr/bin/python3
 
 import turtle
-import time
-
-
-def ship_stop():
-    """
-    Funcion para detener el movimiento de la nave.
-    Actualmente no se usa.
-    """
-    ship.move = "stop"
 
 
 def ship_right():
     """
-    Funcion para para cambiar el estado de la nave a moverse a la dereha.
+    Funcion para que la nave se mueva a la dereha.
     Actualmente se activa con la flecha derecha.
     """
-    ship.move = "right"
+    #ship.move = "right"
+    x = ship.xcor()  # Coordenada x actual
+    if x >= 230:
+        ship.setx(230)  # Condición para no salirse del borde derecho
+    else:
+        ship.setx(x + 10)  # Mueve la nave 10 pixeles a la derecha
 
 
 def ship_left():
     """
-    Funcion para para cambiar el estado de la nave a moverse a la izquierda.
+    Funcion para para que la nave se mueva a la izquierda.
     Actualmente se activa con la flecha izquierda.
     """
-    ship.move = "left"
-
-
-def ship_move(ship):
-    """"
-    Funcion para mover la nave.
-    Actualiza las coordenadas de la nave según su estado.
-    """
+    # ship.move = "left"
     x = ship.xcor()  # Coordenada x actual
-    if ship.move == "right":
-        ship.setx(x + 2.5)  # Mueve la nave 2.5 pixeles a la derecha
-    elif ship.move == "left":
-        ship.setx(x - 2.5)  # Mueve la nave 2.5 pixeles a la izquierda
-
+    if x <= -230:
+        ship.setx(-230)  # Condición para no salirse del borde izquierdo
+    else:
+        ship.setx(x - 10)  # Mueve la nave 10 pixeles a la izquierda
 
 if __name__ == "__main__":
-    # Retardo para relentizar las animaciones
-    delay = 0.01
-
     # Creación de la ventana del juego
     window = turtle.Screen()
     window.title("Alien Invaders")
@@ -59,18 +44,14 @@ if __name__ == "__main__":
     ship.left(90)
     ship.penup()
     ship.goto(0, -240)  # Posicion inicial al centro y abajo
-    ship.move = "stop"  # Estado de movimiento inicial
 
-    # Para que la ventana rastrre los inputs del teclado
+    # Para que la ventana rastree los inputs del teclado
     window.listen()
 
     # Asociacion del teclado con el movimiento de la nave
     window.onkeypress(ship_right, "Right")
     window.onkeypress(ship_left, "Left")
-    window.onkeyrelease(ship_stop, "Right")
-    window.onkeyrelease(ship_stop, "Left")
+    
 
     while True:
         window.update()
-        ship_move(ship)
-        time.sleep(delay)
