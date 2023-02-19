fb3ddc955f74b2b4ed2292fd346095dd85eae001
pizarrin737 <jmpv1924@gmail.com>
2023-02-19 14:32:00 -0600

    Se agrego la animación de las explosiones y los efectos de sonido

--- Proyecto.py
+++ Proyecto.py
@@ -2,6 +2,58 @@
 
 import turtle
 from random import randint
+import os
+import platform
+
+
+class explosion(turtle.Turtle):
+    """
+    se declara una clase con la cual se crea un objeto tipo turtle
+    usando el metodo inicializador, el cual va a tener las caracteristicas
+    especificadas, como la forma, que se le da usando el contenedor visual
+    .frame en donde se almacena una serie de imagenes que generan el efecto
+    visual de explosion
+    """
+
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.shape("1.gif")
+        self.frame1 = 0
+        self.frame = [
+            "1.gif", "2.gif", "3.gif", "4.gif", "5.gif",
+            "6.gif", "7.gif", "8.gif", "9.gif", "10.gif", "11.gif", "12.gif",
+            "13.gif", "14.gif", "14.gif", "15.gif", "16.gif", "17.gif",
+            "18.gif", "19.gif", "20.gif", "21.gif", "22.gif", "23.gif",
+            "24.gif", "25.gif"
+            ]
+
+    def animacion(self):
+        """
+        funcion se que recorre el frame con un ciclo y ademas,
+        muestra la imagens a una velocidad especificada con el metodo
+        ontimer
+        """
+        if self.frame1 < len(self.frame)-1:
+            self.frame1 += 1
+        self.shape(self.frame[self.frame1])
+        window.ontimer(self.animacion, 18)
+        self.clear()
+
+
+def sounds(sound_file, time=0):  # time = 0 inmediatos
+    # sounds("BOOM.wav") para colisión bala-alien y alien-nave
+    # sounds("BULLET.wav") para bala al ser disparada
+    # sounds("BACKGROUND.wav", 16) para sonido de fondo al iniciar juego
+    # Importante que archivos de sonido estén en la misma carpeta
+    # TODOS LOS SONIDOS SON DE LIBRE USO. https://orangefreesounds.com/
+    if platform.system() == "Linux":
+        os.system("aplay -q {}&".format(sound_file))
+    # & para no pausar el juego al reproducirse sonido
+    elif time > 0:
+        # time(s): tiempo que dura el sonido.
+        # t(ms): tiempo en volver a reproducirse.
+        turtle.ontimer(lambda: sounds(sound_file, time), t=int(time*1000))
 
 
 def ship_right():
@@ -42,6 +94,9 @@ def weapon_in_wait():
 
     # Solo dispara si el proyectil está disponible
     if avaiable is True:
+
+        # Sonido del proyectil
+        sounds("PIUM.wav")
 
         # Regresa el proyectil a la punta de la nave
         proyectile.goto(ship.xcor(), ship.ycor()+10)
@@ -127,8 +182,10 @@ def colisiones(alien, proyectile):
     alien: Objeto.turtle() correspondiente a el proyectil del usuario.
     """
 
-    # Variable GLOBAL necesaria para trackear el estado del proyectil
-    global avaiable
+    # Variable GLOBAL avaiable para trackear el estado del proyectil
+    # Variable GLOBAL score para trackear la puntuación
+    # Variable GLOBAL high_score para trackear la puntuación máxima
+    global avaiable, score, high_score
 
     # Delimitación del hitbox del enemigo (20x20 pixeles actualemnte)
     if (proyectile.xcor() >= alien.xcor()-10  # limite izquierdo
@@ -138,12 +195,31 @@ def colisiones(alien, proyectile):
             proyectile.ycor() >= alien.ycor()-10  # Limite inferior
             and proyectile.ycor() <= alien.ycor()+10):  # Limite superior
 
+        # Sonido de destrucción del enemigo
+        sounds("BOOM.wav")
+
+        explo = explosion()  # se llama la clase para crear la explosion
+        explo.goto(alien.xcor(), alien.ycor())  # coloca la explosio en el
+        # mismo lugar donde se intersecan el proyectil y el alien
+        explo.animacion()  # llama la animacion visual de explosion
+
         # Cuando un enemigo es golpeado este se mueve arriba de la ventana
         x = randint(-230, 230)  # Nueva coordenada x (dentro de ventana)
         alien.goto(x, 260)  # Nueva posición del enemigo
         avaiable = True  # El proyectil vuelve a estar disponible
         proyectile.goto(0, -300)  # Proyectil enviado bajo la ventana
         proyectile.hideturtle()  # Se esconde el proyectil
+
+        # Actualiza el marcador del jugador
+        score += 100
+        if score >= high_score:
+            high_score = score
+        puntaje.clear()
+        puntaje.write("Score: {}        HighScore: {}".format(
+            score, high_score
+            ),
+            align="center", font=("Courier",  24, "normal")
+            )
 
 
 def Game_Over(alien):
@@ -155,24 +231,111 @@ def Game_Over(alien):
     alien: Objeto.turtle() correspondiente a un enemigo.
     """
 
-    # Variable GLOBAL necesaria para trackear el estado de la partida
-    global GameOver
+    # Variable GLOBAL GameOver para trackear el estado de la partida
+    # Variable GLOBAL score para trackear la puntuación
+    # Variable GLOBAL high_score para trackear la puntuación máxima
+    global GameOver, score, high_score
 
     # Situación de Game Over
     if alien.ycor() < ship.ycor():
         GameOver = True
+        # Resetea el score para la proxima partida
+        score = 0
+        puntaje.clear()
+        puntaje.write("Score: {}        HighScore: {}".format(
+            score, high_score
+            ),
+            align="center", font=("Courier",  24, "normal")
+            )
+
+
+def click(x, y):
+    """
+    Función empleada para manejar el menu principal del juego.
+    Define un hitbox en el cual el mouse puede tocar los botones
+
+    Actualmente se activa con el click derecho del mouse.
+    """
+
+    # Variable GLOBAL para iniciar la partida
+    global start
+
+    # Condición para iniciar partida
+    if x > 0 and x < 220 and y > 0 and y < 60:
+        # Cambia el color de la pantalla
+        window.bgcolor("black")
+
+        # Vuelve visibles los objetos
+        ship.showturtle()
+        alien1.showturtle()
+        alien2.showturtle()
+        alien3.showturtle()
+        alien4.showturtle()
+        alien5.showturtle()
+        alien6.showturtle()
+        alien7.showturtle()
+        alien8.showturtle()
+
+        # Condición de inicio de partida
+        start = True
+    # Condición para cerrar el programa
+    elif x > 0 and x < 220 and y > -60 and y < 0:
+        start = False
+    else:
+        click(x, y)  # Hace que no pase nada en caso de click fuera de marcos
+
+
+def NewGamePlus(alien):
+    """
+    Función empleada para reacomodar a los enemigos para empezar
+    una nueva partida
+    """
+    x = randint(-230, 230)  # Coordenada x inicial aleatoria
+    alien.penup()
+    alien.hideturtle()  # Esconde al enemigo
+    alien.goto(x, 260)  # Devulve al enemigo arriba en la pantalla
+    alien.speed(0)
+    alien.shape("triangle")
+    alien.color("green")
 
 
 if __name__ == "__main__":
+
     # Creación de la ventana del juego
     window = turtle.Screen()
     window.title("Alien Invaders")
     window.setup(width=500, height=500)  # Unidades en pixeles
-    window.bgcolor("black")
+    window.bgcolor("white")
     window.tracer(0)
+
+    # Se usa para crear los botones del menu
+    pen = turtle.Turtle()
+    pen.hideturtle()
+
+    # Crea marco para las opciones del menu
+    for i in range(2):
+        pen.forward(220)
+        pen.left(90)
+        pen.forward(60)
+        pen.left(90)
+    pen.goto(0, -60)
+    for i in range(2):
+        pen.forward(220)
+        pen.left(90)
+        pen.forward(60)
+        pen.left(90)
+
+    pen.penup()  # Escribe opción para iniciar juego
+    pen.goto(14, 12)
+    pen.write("START GAME", font=("Courier",  24, "normal"))
+
+    pen.penup()  # Escribe opción para salir del juego
+    pen.goto(14, -42)
+    pen.write("EXIT", font=("Courier",  24, "normal"))
 
     # Creación de la nave del usuario
     ship = turtle.Turtle()
+    ship.hideturtle()
     ship.speed(0)
     ship.shape("arrow")
     ship.color("white")
@@ -181,7 +344,6 @@ if __name__ == "__main__":
     ship.goto(0, -240)  # Posicion inicial al centro y abajo
 
     # Creación del proyectil que dispara el usuario
-    # Fue necesario sacarlo de la función ya que sino el loop principal crashea
     proyectile = turtle.Turtle()
     proyectile.hideturtle()  # Inicialmente oculto (No ha disparado)
     proyectile.speed(0)
@@ -194,140 +356,185 @@ if __name__ == "__main__":
                     ship.ycor()+10)  # Ubicado en la punta de la nave
     avaiable = True  # Disponibilidad del proyectil. Disponible al inicio
 
+    # se importan todas las imagenes que se
+    # usan para crear la explosion
+    window.addshape("1.gif")
+    window.addshape("2.gif")
+    window.addshape("3.gif")
+    window.addshape("4.gif")
+    window.addshape("5.gif")
+    window.addshape("6.gif")
+    window.addshape("7.gif")
+    window.addshape("8.gif")
+    window.addshape("9.gif")
+    window.addshape("10.gif")
+    window.addshape("11.gif")
+    window.addshape("12.gif")
+    window.addshape("13.gif")
+    window.addshape("14.gif")
+    window.addshape("15.gif")
+    window.addshape("16.gif")
+    window.addshape("17.gif")
+    window.addshape("18.gif")
+    window.addshape("19.gif")
+    window.addshape("20.gif")
+    window.addshape("21.gif")
+    window.addshape("22.gif")
+    window.addshape("23.gif")
+    window.addshape("24.gif")
+    window.addshape("25.gif")
+
     # Creación de los enemigos
 
     # Enemigo 1
-    x1 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien1 = turtle.Turtle()
-    alien1.speed(0)
-    alien1.shape("triangle")
-    alien1.color("green")
-    alien1.right(90)
-    alien1.penup()
-    alien1.goto(x1, 260)  # Posicion inicial
     alien1.direction = 1  # Inicialmente se mueve a la derecha
+    alien1.right(90)
+    NewGamePlus(alien1)
 
     # Enemigo 2
-    x2 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien2 = turtle.Turtle()
-    alien2.speed(0)
-    alien2.shape("triangle")
-    alien2.color("green")
-    alien2.right(90)
-    alien2.penup()
-    alien2.goto(x2, 260)  # Posicion inicial al centro y abajo
     alien2.direction = -1  # Inicialmente se mueve a la izquierda
+    alien2.right(90)
+    NewGamePlus(alien2)  # Posicion inicial al centro y abajo
 
     # Enemigo 3
-    x3 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien3 = turtle.Turtle()
-    alien3.speed(0)
-    alien3.shape("triangle")
-    alien3.color("green")
-    alien3.right(90)
-    alien3.penup()
-    alien3.goto(x3, 260)  # Posicion inicial
     alien3.direction = 1  # Inicialmente se mueve a la derecha
+    alien3.right(90)
+    NewGamePlus(alien3)
 
     # Enemigo 4
-    x4 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien4 = turtle.Turtle()
-    alien4.speed(0)
-    alien4.shape("triangle")
-    alien4.color("green")
-    alien4.right(90)
-    alien4.penup()
-    alien4.goto(x4, 260)  # Posicion inicial al centro y abajo
     alien4.direction = -1  # Inicialmente se mueve a la izquierda
+    alien4.right(90)
+    NewGamePlus(alien4)  # Posicion inicial al centro y abajo
 
     # Enemigo 5
-    x5 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien5 = turtle.Turtle()
-    alien5.speed(0)
-    alien5.shape("triangle")
-    alien5.color("green")
-    alien5.right(90)
-    alien5.penup()
-    alien5.goto(x5, 260)  # Posicion inicial
     alien5.direction = 1  # Inicialmente se mueve a la derecha
+    alien5.right(90)
+    NewGamePlus(alien5)
 
     # Enemigo 6
-    x6 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien6 = turtle.Turtle()
-    alien6.speed(0)
-    alien6.shape("triangle")
-    alien6.color("green")
-    alien6.right(90)
-    alien6.penup()
-    alien6.goto(x6, 260)  # Posicion inicial al centro y abajo
     alien6.direction = -1  # Inicialmente se mueve a la izquierda
+    alien6.right(90)
+    NewGamePlus(alien6)  # Posicion inicial al centro y abajo
 
     # Enemigo 7
-    x7 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien7 = turtle.Turtle()
-    alien7.speed(0)
-    alien7.shape("triangle")
-    alien7.color("green")
-    alien7.right(90)
-    alien7.penup()
-    alien7.goto(x7, 260)  # Posicion inicial
     alien7.direction = 1  # Inicialmente se mueve a la derecha
+    alien7.right(90)
+    NewGamePlus(alien7)
 
     # Enemigo 8
-    x8 = randint(-230, 230)  # Coordenada x inicial aleatoria
     alien8 = turtle.Turtle()
-    alien8.speed(0)
-    alien8.shape("triangle")
-    alien8.color("green")
-    alien8.right(90)
-    alien8.penup()
-    alien8.goto(x8, 260)  # Posicion inicial al centro y abajo
     alien8.direction = -1  # Inicialmente se mueve a la izquierda
+    alien8.right(90)
+    NewGamePlus(alien8)  # Posicion inicial al centro y abajo
 
     # Para que la ventana rastree los inputs del teclado
     window.listen()
+
+    # Asociacion del mouse con el menu
+    window.onscreenclick(click, 1)
 
     # Asociacion del teclado con el movimiento de la nave
     window.onkeypress(ship_right, "Right")  # Derecha
     window.onkeypress(ship_left, "Left")  # Izquierda
     window.onkeypress(weapon_in_wait, "space")  # Disparo
 
+    # Variables que almacenan la puntuación
+    score = 0
+    high_score = 0
+
+    # Creación del marcador en pantalla
+    puntaje = turtle.Turtle()  # Parte visual
+    puntaje.speed(0)  # Probar otros valores para crear animación
+    puntaje.color("white")  # Depende de color e pantalla final
+    puntaje.penup()  # Oculta lápiz, podría quitarse para animación
+    puntaje.hideturtle()
+    puntaje.goto(0, 300)  # Ver donde ponerlo sin que moleste visualmente
+    puntaje.write("Score: {}        HighScore: {}".format(
+            score, high_score
+            ),
+            align="center", font=("Courier",  24, "normal")
+            )
+
     # Variable que indica si el jugador perdio o no
     GameOver = False  # El jugador no inicia perdiendo
 
-    while GameOver is False:
-        # Refresca la ventana
-        window.update()
+    # Variable que indica el estado del programa
+    start = None  # Inicialmente indefinido
 
-        # Anima el proyectil
-        weapon_in_use()
+    # Loop principal del programa
+    while True:
 
-        # Anima los enemigos
-        alien_move(alien1)
-        alien_move(alien2)
-        alien_move(alien3)
-        alien_move(alien4)
-        alien_move(alien5)
-        alien_move(alien6)
-        alien_move(alien7)
-        alien_move(alien8)
+        # Permite al usuario decidir que hacer desde el menu
+        while start is None:
+            window.update()
 
-        # Colisiones enemigo-proyectil
-        colisiones(alien1, proyectile)
-        colisiones(alien2, proyectile)
-        colisiones(alien3, proyectile)
-        colisiones(alien4, proyectile)
-        colisiones(alien5, proyectile)
-        colisiones(alien6, proyectile)
-        colisiones(alien7, proyectile)
-        colisiones(alien8, proyectile)
+        # Se activa si el usuario decide iniciar una partida
+        if start is True:
 
-        # Verifica si el jugador no ha perdido
-        Game_Over(alien1)
-        Game_Over(alien2)
-        Game_Over(alien3)
-        Game_Over(alien4)
-        Game_Over(alien5)
-        Game_Over(alien6)
-        Game_Over(alien7)
-        Game_Over(alien8)
+            # Loop principal de juego
+            while GameOver is False:
+                # Refresca la ventana
+                window.update()
+
+                # Anima el proyectil
+                weapon_in_use()
+
+                # Anima los enemigos
+                alien_move(alien1)
+                alien_move(alien2)
+                alien_move(alien3)
+                alien_move(alien4)
+                alien_move(alien5)
+                alien_move(alien6)
+                alien_move(alien7)
+                alien_move(alien8)
+
+                # Colisiones enemigo-proyectil
+                colisiones(alien1, proyectile)
+                colisiones(alien2, proyectile)
+                colisiones(alien3, proyectile)
+                colisiones(alien4, proyectile)
+                colisiones(alien5, proyectile)
+                colisiones(alien6, proyectile)
+                colisiones(alien7, proyectile)
+                colisiones(alien8, proyectile)
+
+                # Verifica si el jugador no ha perdido
+                Game_Over(alien1)
+                Game_Over(alien2)
+                Game_Over(alien3)
+                Game_Over(alien4)
+                Game_Over(alien5)
+                Game_Over(alien6)
+                Game_Over(alien7)
+                Game_Over(alien8)
+
+            NewGamePlus(alien1)
+            NewGamePlus(alien2)
+            NewGamePlus(alien3)
+            NewGamePlus(alien4)
+            NewGamePlus(alien5)
+            NewGamePlus(alien6)
+            NewGamePlus(alien7)
+            NewGamePlus(alien8)
+
+            # esconde a la nave del jugador
+            ship.hideturtle()
+
+            # Cambia el color de la ventana
+            window.bgcolor("white")
+
+            # Reseteo de las variables
+            start = None  # Cambia el estado del programa a indefinido
+            GameOver = False  # Permite al jugador iniciar otra partida
+
+        # Se activa si el usuario decide cerrar el programa
+        else:
+            break
