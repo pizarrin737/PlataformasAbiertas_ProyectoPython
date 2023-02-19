44d21fe149b2bb86672b276c89f3b85119f42895
C18390 <124937296+C18390@users.noreply.github.com>
2023-02-18 23:12:45 -0600

    Update SonidosTests.py
    v.2 para funciones de sonido.

--- SonidosTests.py
+++ SonidosTests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+
 """Función para sonido de fondo, balas y explosión por colisión"""
+
 
 # Imports requeridos
 import platform
@@ -8,17 +10,18 @@ import os
 import turtle
 
 # Función
-def sounds(sound_file, time = 0): # time = 0 inmediatos
+
+
+def sounds(sound_file, time=0):  # time = 0 inmediatos
     if platform.system() == "Linux":
-        os.system("aplay -q {}&".format(sound_file)) 
-        # & para no pausar el juego al reproducirse sonido
-    if time > 0:
+        os.system("aplay -q {}&".format(sound_file))
+# & para no pausar el juego al reproducirse sonido
+    elif time > 0:
         # time(s): tiempo que dura el sonido.
         # t(ms): tiempo en volver a reproducirse.
         turtle.ontimer(lambda: sounds(sound_file, time), t=int(time*1000))
-        
+
 # sounds("BOOM.wav") para colisión bala-alien y alien-nave
 # sounds("BULLET.wav") para bala al ser disparada
 # sounds("BACKGROUND.wav", 16) para sonido de fondo al iniciar juego
-
 # Importante que archivos de sonido estén en la misma carpeta
